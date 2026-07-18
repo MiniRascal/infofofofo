463006595e8e515efb107ab7feab071c186f491f
MiniRascal <mininotrascal@gmail.com>
2026-07-18 01:25:36 +0100

    Update GamakayDriverClient.cs

--- Gamakay/GamakayDriverClient.cs
+++ Gamakay/GamakayDriverClient.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 using InfoPanel.PeripheralBattery.Gamakay.Proto;
